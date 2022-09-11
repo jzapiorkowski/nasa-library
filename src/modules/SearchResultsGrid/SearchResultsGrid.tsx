1ed5d094101988b4dfa7313902145821c1d9530d
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-09-11 14:48:02 +0200

    display grid with results

--- src/modules/SearchResultsGrid/SearchResultsGrid.tsx
+++ src/modules/SearchResultsGrid/SearchResultsGrid.tsx
@@ -1,10 +1,18 @@
-import { Grid } from '@mui/material';
+import { Grid, Paper, Box, styled } from '@mui/material';
 import { Loader } from 'components';
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { StateType } from 'store';
 
+const StyledGridContainer = styled(Grid)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 40px 0;
+`;
+
 interface StateProps {
-  data: Record<string, {}>[];
+  data: Record<string, Record<string, any>[]>[];
   isLoading: boolean;
 }
 
@@ -14,9 +22,47 @@ export function SearchResultsGrid() {
     isLoading: state.searchResults.isLoading,
   }));
 
+  const preparedResultsItems = useMemo(
+    () =>
+      data.map((element) => {
+        return (
+          <Grid item key={element.data[0].nasa_id} sx={{ width: 400 }}>
+            <Paper
+              elevation={6}
+              sx={{
+                width: '100%',
+                paddingTop: 1,
+                paddingBottom: 1,
+              }}
+            >
+              {element.data[0].title}
+            </Paper>
+            <Box
+              component='img'
+              sx={{
+                height: 300,
+                width: '100%',
+              }}
+              alt=''
+              src={element.links[0].href}
+            />
+          </Grid>
+        );
+      }),
+    [data]
+  );
+
   if (isLoading) {
     return <Loader />;
   }
 
-  return <Grid container></Grid>;
+  return (
+    <StyledGridContainer
+      container
+      rowSpacing={1}
+      columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+    >
+      {preparedResultsItems}
+    </StyledGridContainer>
+  );
 }

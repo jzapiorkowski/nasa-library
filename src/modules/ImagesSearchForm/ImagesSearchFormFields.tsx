d798362f090626614319fba7bcd600bd372ac9b2
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-09-11 19:25:09 +0200

    add year end & start fields to form

--- src/modules/ImagesSearchForm/ImagesSearchFormFields.tsx
+++ src/modules/ImagesSearchForm/ImagesSearchFormFields.tsx
@@ -3,6 +3,10 @@ import { FormValues } from './types';
 
 export function ImagesSearchFormFields() {
   return (
-    <TextField name={FormValues.QueryInput} label='Query Input' isRequired />
+    <>
+      <TextField name={FormValues.QueryInput} label='Query Input' isRequired />
+      <TextField name={FormValues.YearStart} label='Year start' type='number' />
+      <TextField name={FormValues.YearEnd} label='Year end' type='number' />
+    </>
   );
 }
--- src/modules/ImagesSearchForm/types.ts
+++ src/modules/ImagesSearchForm/types.ts
@@ -1,7 +1,11 @@
 export enum FormValues {
   QueryInput = 'QueryInput',
+  YearStart = 'YearStart',
+  YearEnd = 'YearEnd',
 }
 
 export interface FormValuesTypes {
   [FormValues.QueryInput]: string;
+  [FormValues.YearStart]: number | '';
+  [FormValues.YearEnd]: number | '';
 }

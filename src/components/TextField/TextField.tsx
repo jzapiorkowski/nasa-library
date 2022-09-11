0d44dedff01a69bd15623f4e4272456c40212be7
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-09-11 19:24:10 +0200

    add error message to textfield component

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,24 +1,38 @@
-import { Field } from 'formik';
-import { TextField as MuiTextField } from '@mui/material';
+import { ErrorMessage, Field } from 'formik';
+import { styled, TextField as MuiTextField, Typography } from '@mui/material';
 import { theme } from 'theme';
+import { HTMLInputTypeAttribute } from 'react';
+
+const StyledErrorMessage = styled(ErrorMessage)`
+  color: ${({ theme }) => theme.palette.error.main};
+`;
 
 interface TextFieldProps {
   name: string;
   label?: string;
   placeholder?: string;
   isRequired?: boolean;
+  type?: HTMLInputTypeAttribute;
 }
 
-export function TextField({ name, label, isRequired = false }: TextFieldProps) {
+export function TextField({
+  name,
+  label,
+  isRequired = false,
+  type = 'text',
+}: TextFieldProps) {
   return (
-    <Field
-      name={name}
-      label={label}
-      as={MuiTextField}
-      type='text'
-      required={isRequired}
-      focused
-      sx={{ input: { color: theme.palette.primary.main } }}
-    />
+    <>
+      <Field
+        name={name}
+        label={label}
+        as={MuiTextField}
+        type={type}
+        required={isRequired}
+        focused
+        sx={{ input: { color: theme.palette.primary.main } }}
+      />
+      <StyledErrorMessage name={name} component={Typography} />
+    </>
   );
 }

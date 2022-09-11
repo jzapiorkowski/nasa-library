798bf35d2ca81113519f5009d804fc23b6a54019
jzapiorkowski <92112490+jzapiorkowski@users.noreply.github.com>
2022-09-11 15:39:56 +0200

    add header (#8)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { PageHeader } from 'modules/PageHeader';
 import { SearchResultsGridContainer } from 'modules/SearchResultsGrid';
 import './App.css';
 import { ImagesSearchForm } from './modules/ImagesSearchForm';
@@ -5,6 +6,7 @@ import { ImagesSearchForm } from './modules/ImagesSearchForm';
 function App() {
   return (
     <div className='App'>
+      <PageHeader />
       <ImagesSearchForm />
       <SearchResultsGridContainer />
     </div>
---
+++ src/modules/PageHeader/PageHeader.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+export function PageHeader() {
+  return (
+    <Typography variant='h2'>
+      The awesome nasa library image searcher
+    </Typography>
+  );
+}
